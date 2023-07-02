dae6351b633ca547bba75ed8a3539942f740ece7
Mauricio Vélez <serketh7@gmail.com>
2023-07-02 02:39:58 -0500

    connected and synchronized front and back with socket

--- node-soket-BF/api/index.js
+++ node-soket-BF/api/index.js
@@ -1,11 +1,36 @@
-const express = require('express');
-const morgan = require('morgan')
+const express = require("express");
+const morgan = require("morgan");
+const cors = require('cors')
+const { Server } = require("socket.io");
+
+const http = require("http");
+
 
 const app = express();
-app.use(morgan('dev'))
+app.use(cors())
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+  }
+});
+
+app.use(morgan("dev"));
 
 app.get('/', (req, res) => {
-  res.send('API');
+  res.send('hello')
 })
 
-app.listen(3001, console.log('en el 3001'))
+io.on('connection', (socket) => {
+  console.log(socket.id); // id del frontend conectado
+  
+  socket.on('message', (message) => { // recibe el mensaje del cliente (submit)
+    console.log(message);
+    socket.broadcast.emit('message', message) // reenvia el mensaje a los otros clientes
+  })
+})
+
+// app.listen(3001, console.log('app en 3001'))
+server.listen(3002, console.log("server en el 3002"));
--- node-soket-BF/client/src/App.tsx
+++ node-soket-BF/client/src/App.tsx
@@ -1,7 +1,44 @@
+import { useEffect, useState } from 'react';
+import io from 'socket.io-client';
 import './App.css';
 
+const socket = io('http://localhost:3002');
+
 function App() {
-  return <></>;
+  const [message, setMessage] = useState('');
+
+  const handleonChange = (e) => {
+    setMessage(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setMessage('');
+    socket.emit('message', message);
+  };
+  useEffect(() => {
+
+    const reciveMessage = (message) => {
+      console.log(message);
+    }
+
+    socket.on('message', reciveMessage); // para refrescar cuando otro cliente mande un mensaje
+  }, []);
+  return (
+    <>
+      <h1>App Socket</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type='text'
+          placeholder='Enter your message...'
+          value={message}
+          onChange={handleonChange}
+        />
+        <button onClick={handleSubmit}>Send Message</button>
+      </form>
+      <label>{message}</label>
+    </>
+  );
 }
 
 export default App;
